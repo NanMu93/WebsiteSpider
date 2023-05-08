38f4051d66fd7b160223d227e63940e9e8fde54f
NanMu93 <mscboom@gmail.com>
2023-05-08 15:25:43 +0800

    Create mian.py

---
+++ spider/mian.py
@@ -0,0 +1,9 @@
+from scrapy.cmdline import execute
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# 如果要运行其他的网络爬虫，只需修改上面代码中字符串里面的命令即可
+execute(["scrapy", "crawl", "demowebsite"])
+# 或者（cmdline.execute("scrapy crawl first_spider -o info.csv -t csv".split())
